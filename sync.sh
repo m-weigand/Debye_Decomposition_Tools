8c4f58b954b064c3d1006b3047859094e5995077
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-07-12 16:35:51 +0200

    move to Gitlab-server

--- sync.sh
+++ sync.sh
@@ -5,7 +5,8 @@
 # https://github.com/m-weigand/GeccoInv
 
 test -d geccoinv && rm -rf geccoinv
-git clone git@github.com:m-weigand/geccoinv.git
+# git clone git@github.com:m-weigand/geccoinv.git
+git clone ssh://git@ssh.geo.uni-bonn.de:4422/mweigand/geccoinv.git
 cd geccoinv
 # commit='f1a83f0e51a4c7c63a31a21f242bec8502bd27cd'
 # git reset --hard ${commit}
@@ -13,7 +14,8 @@ rm -fr .git
 cd ..
 
 test -d dd_interfaces && rm -rf dd_interfaces
-git clone git@github.com:m-weigand/dd_interfaces.git
+# git clone git@github.com:m-weigand/dd_interfaces.git
+git clone ssh://git@ssh.geo.uni-bonn.de:4422/mweigand/dd_interfaces.git
 cd dd_interfaces
 # commit='b278a19b09f79374aca7dd90e0eb63392ee901d3'
 # git reset --hard ${commit}
