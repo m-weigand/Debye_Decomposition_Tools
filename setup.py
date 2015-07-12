5a658881f8cad8416e16310edc3afd7c438e2603
Maximilian Weigand <mweigand@geo.uni-bonn.de>
2015-07-12 16:35:31 +0200

    add global setup.py file, which installs both geccoinv und dd_interfaces

---
+++ setup.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+from setuptools import setup
+import os
+import subprocess
+# from setuptools import find_packages
+# find_packages
+
+# under windows, run
+# python.exe setup.py bdist --format msi
+# to create a windows installer
+
+# TODO: understand the inclusion of requirements
+
+version_short = '0.5'
+version_long = '0.5.4'
+# if we are in a git directory, use the last git commit as the version
+cmd = 'git log -1 --format=%H'
+try:
+    if os.path.isdir('.git'):
+        git_output = subprocess.check_output(cmd, shell=True).strip()
+        version_long += '+' + git_output
+except:
+    pass
+
+if __name__ == '__main__':
+    setup(name='dd_tools',
+          version=version_long,
+          description='Debye decomposition tools',
+          author='Maximilian Weigand',
+          author_email='mweigand@geo.uni-bonn.de',
+          url='http://www.geo.uni-bonn.de',
+          # find_packages() somehow does not work under Win7 when creating a
+          # msi # installer
+          # packages=find_packages(),
+          package_dir={'': 'geccoinv/lib',
+                       'lib_dd': 'dd_interfaces/lib',
+                       'lib_dd.conductivity': 'dd_interfaces/lib',
+                       'lib_dd.io': 'dd_interfaces/lib',
+                       },
+          packages=['lib_cc',
+                    'lib_cc2',
+                    'lib_cc_conductivity',
+                    'NDimInv',
+                    'sip_formats',
+                    'lib_dd',
+                    'lib_dd.conductivity',
+                    'lib_dd.io'],
+          scripts=['dd_interfaces/src/dd_single/dd_single.py',
+                   'dd_interfaces/src/dd_time/dd_time.py',
+                   'dd_interfaces/src/dd_space_time/dd_space_time.py',
+                   'dd_interfaces/src/dd_test/dd_test.py',
+                   'dd_interfaces/src/ddps/ddps.py',
+                   'dd_interfaces/src/ddpt/ddpt.py',
+                   'dd_interfaces/src/ddpst/ddpst.py'],
+          install_requires=['numpy', 'scipy>=0.12', 'matplotlib']
+          )
